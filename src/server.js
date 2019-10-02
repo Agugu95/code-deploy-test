578827c71b8cfec6ab55602879295b1413372834
Ubuntu <ubuntu@ip-172-31-22-33.ap-northeast-2.compute.internal>
2019-10-02 09:17:07 +0000

    fuck

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const koa = require('koa');
-
-const router = require('./router');
-
-const app = new koa();
-app.use(router.routes());
-
-app.listen(3000, () => { console.info('Server is running on port 3000'); });
